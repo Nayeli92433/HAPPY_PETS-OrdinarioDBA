13604b219eb3b54d8020ad8885d2ea12142ce1a8
nayeli92433 <nayeserratvl@gmail.com>
2025-02-06 11:40:37 -0600

    Se agregaron mas archivos (No usarlos)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Inicio from './pages/Home';
+import Home from './pages/Home';
 import Servicios from './pages/Servicios';
 import Galeria from './pages/Galeria';
 import Admin from './pages/Admin';
+import Citas from './pages/CrearCita'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/" element={<Home />} />
         <Route path="/servicios" element={<Servicios />} />
         <Route path="/galeria" element={<Galeria />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/citas" element={<Citas />} />
       </Routes>
     </Router>
   );
---
+++ src/Utils/Data.js
@@ -0,0 +1,17 @@
+// Array de servicios
+export const services = [
+    "Consulta general",
+    "Vacunación",
+    "Desparacitación",
+    "Emergencia",
+    "Revisión de salud"
+  ];
+  
+  // Array de veterinarios
+  export const veterinarians = [
+    "Dr. Juan Pérez",
+    "Dra. Laura Gómez",
+    "Dr. Ricardo Martínez",
+    "Dra. Ana Torres"
+  ];
+  
